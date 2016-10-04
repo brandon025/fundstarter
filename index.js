045bc1332e55b9d6089309b761a8772c064d0073
brandon025 <brandon025@gmail.com>
2016-10-03 22:58:57 -0700

    test

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-var express = require('express')
-var app = express()
 var fs = require('fs');
 
-app.set('port', (process.env.PORT || 8080))
-
-fs.readFile('public/index.html', function (err, html){
+fs.readFile('public/index.html', function (err, html)){
             if (err){
                 throw err;
             }
@@ -12,22 +8,5 @@ fs.readFile('public/index.html', function (err, html){
                 response.writeHeader(200, {"Content-Type": "text/html"});
                 response.write(html);
                 response.end();
-            })
-})
-//app.use(express.static(__dirname + '/public'))
-
-//__dirname returns the directory that the currently executing script is in.
-
-// app.get('/', function(request, response) {
-   // response.sendFile('public/index.html',{root:__dirname})
-
-/* sends an entire HTTP response to the client,                                                                                                                                     
- including headers and content,                                                                                                                                                     
- which is why you can only call once*/
-
-
-// })
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at :" + app.get('port'))
+            }).listen(8080);
 })
