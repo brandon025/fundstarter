ea21bf505ed8e6337512a0e3abe8bde5aae3fb68
brandon025 <brandon025@gmail.com>
2016-10-03 22:53:12 -0700

    4

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
 var express = require('express')
 var app = express()
-var fs = require("fs");
-var filename = "public/index.html"
-
-var buffer = fs.readFileSync(filename, "utf8");
-
+var fs = require('fs');
 
 app.set('port', (process.env.PORT || 8080))
 
-function start(resp){
-    resp.writeHead(200, {"Content-type":"text/html"});
-    resp.write(buffer);
-    resp.end();
-}
-
-exports.start=start;
+fs.readFile('public/index.html', function (err, html)
+            if (err){
+                throw err;
+            }
+            http.createServer(function(request,response) {
+                response.writeHeader(200, {"Content-Type": "text/html"});
+                response.write(html);
+                response.end();
+            })
+})
 //app.use(express.static(__dirname + '/public'))
 
 //__dirname returns the directory that the currently executing script is in.
 
-app.get('/', function(request, response) {
-    response.sendFile('public/index.html',{root:__dirname})
+// app.get('/', function(request, response) {
+   // response.sendFile('public/index.html',{root:__dirname})
 
 /* sends an entire HTTP response to the client,                                                                                                                                     
  including headers and content,                                                                                                                                                     
  which is why you can only call once*/
 
 
-})
+// })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at :" + app.get('port'))
