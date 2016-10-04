ca0d2c7cf9da7f03ad4ea8adb3bbcf75d34212f9
brandon025 <brandon025@gmail.com>
2016-10-03 22:59:50 -0700

    restore

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-var fs = require('fs');
+var express = require('express')
+var app = express()
 
-fs.readFile('public/index.html', function (err, html)){
-            if (err){
-                throw err;
-            }
-            http.createServer(function(request,response) {
-                response.writeHeader(200, {"Content-Type": "text/html"});
-                response.write(html);
-                response.end();
-            }).listen(8080);
+
+app.set('port', (process.env.PORT || 8080))
+//app.use(express.static(__dirname + '/public'))
+
+//__dirname returns the directory that the currently executing script is in.
+
+app.get('/', function(request, response) {
+    response.sendFile('public/index.html',{root:__dirname})
+
+/* sends an entire HTTP response to the client,                                                                                                                                     
+ including headers and content,                                                                                                                                                     
+ which is why you can only call once*/
+
+
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at :" + app.get('port'))
 })
