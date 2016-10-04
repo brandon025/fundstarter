195074ff176f1275ac7d456ec452db85df0d9ef4
brandon025 <brandon025@gmail.com>
2016-10-03 22:45:14 -0700

    Change 2

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 var express = require('express')
 var app = express()
 var fs = require("fs");
+var filename = "public/index.html"
+
+var buffer = fs.readFileSync(filename, "utf8");
 
 
 app.set('port', (process.env.PORT || 8080))
+
+function start(resp){
+    resp.writeHead(200, {"Content-type":"text/html"});
+    resp.write(buffer);
+    resp.end();
+}
+
+exports.start=start;
 //app.use(express.static(__dirname + '/public'))
 
 //__dirname returns the directory that the currently executing script is in.
 
-    var data = fs.readFileSync('public/index.html');
-    console.log(data.toString());
-    console.log("Program Ended")
 app.get('/', function(request, response) {
+    response.sendFile('public/index.html',{root:__dirname})
 
 /* sends an entire HTTP response to the client,                                                                                                                                     
  including headers and content,                                                                                                                                                     
