d7a1e7cfc5b5788682c10e76fcc6ae0a0108f8b5
brandon025 <brandon025@gmail.com>
2016-10-10 23:16:01 -0700

    Assignment 2 Start

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var express = require('express')
+/* var express = require('express')
 var app = express()
 var fs=require('fs');
 var buffer= new Buffer(5024)
@@ -33,3 +33,82 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at :" + app.get('port'))
 })
+*/ 
+
+/* var express = require('express')
+var app = express()
+
+
+app.set('port', (process.env.PORT || 8080))
+//app.use(express.static(__dirname + '/public'))
+
+//__dirname returns the directory that the currently executing script is in.
+
+app.get('/', function(request, response) {
+    response.sendFile('public/index.html',{root:__dirname})
+
+/* sends an entire HTTP response to the client,                                                                                                                                     
+ including headers and content,                                                                                                                                                     
+ which is why you can only call once
+
+})
+ 
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at :" + app.get('port'))
+})
+*/
+
+
+var monday = [
+
+        {
+
+            'name'     : 'Write a tutorial',
+
+            'duration' : 180
+
+        },
+
+        {
+
+            'name'     : 'Some web development',
+
+            'duration' : 120
+
+        }
+
+    ];
+
+ 
+
+var tuesday = [
+
+        {
+
+            'name'     : 'Keep writing that tutorial',
+
+            'duration' : 240
+
+        },
+
+        {
+
+            'name'     : 'Some more web development',
+
+            'duration' : 180
+
+        },
+
+        {
+
+            'name'     : 'A whole lot of nothing',
+
+            'duration'  : 240
+
+        }
+
+    ];
+
+     
+
+var tasks = [monday, tuesday];
